ea5416f11253edca3e356715f111d2e11a6bfc47
Justin Peel <justinpeel25@gmail.com>
2020-04-23 12:01:29 +0200

    Initial Commit

---
+++ MiddleCharactersOfString/MiddleCharactersOfStrings.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace MiddleCharactersOfString
+{
+	[TestFixture]
+	public class MiddleCharactersOfStrings
+	{
+		[Test]
+		public void GivenString_FourCharactersLong_ShouldReturnMiddleTwoChars()
+		{
+			//arrange
+			var fullString = "test";
+			//act
+			var expected = "es";
+			var actual = GetMiddleChars(fullString);
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GivenString_WithOddNumberOfChars_ShouldReturnMiddleChar()
+		{
+			//arrange
+			var fullString = "testing";
+			//act
+			var expected = "t";
+			var actual = GetMiddleChars(fullString);
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GivenString_WithEvenNumberOfChars_ShouldReturnMiddleTwoChars()
+		{
+			//arrange
+			var fullString = "middle";
+			//act
+			var expected = "dd";
+			var actual = GetMiddleChars(fullString);
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GivenString_WithOneChar_ShouldReturnTheSingularChar()
+		{
+			//arrange
+			var fullString = "A";
+			//act
+			var expected = "A";
+			var actual = GetMiddleChars(fullString);
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		private object GetMiddleChars(string fullString)
+		{
+			if (fullString.Length == 1)
+				return fullString;
+
+			return (fullString.Length % 2 == 0 && fullString.Length >= 1) 
+					? fullString.Substring((fullString.Length / 2) - 1, 2) 
+					: fullString.Substring((fullString.Length / 2),1);
+		}
+	}
+}
---
+++ MiddleCharactersOfString/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MiddleCharactersOfString")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MiddleCharactersOfString")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29ee1e6b-a989-430f-b97d-14dd1e5ee54a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
